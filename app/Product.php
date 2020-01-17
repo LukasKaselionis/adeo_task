4973bc720aec51202cbda8be558c5268c4018883
LukasKaselionis <l.kaselionis@gmail.com>
2020-01-17 12:31:53 +0200

    init product crud

---
+++ app/Product.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Product
+ * @package App
+ */
+class Product extends Model
+{
+    /**
+     * @var array
+     */
+    protected $fillable = [
+        'title',
+        'sku',
+        'status',
+        'base_price',
+        'special_price',
+        'description'
+    ];
+}
